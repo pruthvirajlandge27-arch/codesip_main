50fc0a73745015bda65301a0a14c2a5b214ba3ab
khushi-jain123 <ktjain1456@gmail.com>
2026-05-13 22:03:27 +0530

    Fix build error in vite.config.js to allow deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,9 +11,6 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', 'framer-motion']
-        }
       }
     }
   }
