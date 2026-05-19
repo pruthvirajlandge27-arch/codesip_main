33c3eba35a922e11c3f0802c5f1a008de1c3fff8
khushi-jain123 <ktjain1456@gmail.com>
2026-05-19 14:16:02 +0530

    Update footer HQ location

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -239,7 +239,7 @@ const MainLayout = () => {
                 </li>
                 <li>
                   <span className="text-[15px] text-[#A0A0A0] block">
-                    Registered HQ: Amravati, Navi Mumbai, Maharashtra, India
+                    Registered HQ: Amravati, Maharashtra, India
                   </span>
                 </li>
               </ul>
